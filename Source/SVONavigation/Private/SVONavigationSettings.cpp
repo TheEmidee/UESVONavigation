de8aba71740cd46ada4106b4e760d1f208c3bf1d
Michael Delva <michael.delva@fishingcactus.com>
2022-01-17 11:53:52 +0100

    Added new option to set the default line of sight class to use

--- Source/SVONavigation/Private/SVONavigationSettings.cpp
+++ Source/SVONavigation/Private/SVONavigationSettings.cpp
@@ -1,7 +1,10 @@
 #include "SVONavigationSettings.h"
 
+#include "Raycasters/SVORaycaster_OctreeTraversal.h"
+
 USVONavigationSettings::USVONavigationSettings()
 {
     ShouldDiscardSubLevelNavigationData = true;
     NavigationAutoUpdateEnabled = true;
+    DefaultLineOfSightClass = USVORayCaster_OctreeTraversal::StaticClass();
 }
--- Source/SVONavigation/Public/SVONavigationSettings.h
+++ Source/SVONavigation/Public/SVONavigationSettings.h
@@ -5,6 +5,7 @@
 
 #include "SVONavigationSettings.generated.h"
 
+class USVORayCaster;
 UCLASS( config = Engine, defaultconfig )
 class SVONAVIGATION_API USVONavigationSettings final : public UDeveloperSettings
 {
@@ -19,4 +20,11 @@ public:
 
     UPROPERTY( config, EditAnywhere, Category = "SVO Navigation" )
     uint8 NavigationAutoUpdateEnabled : 1;
+
+    // The algorithm to use to detect if there's a direct line of sight between the start of tha path and the target
+    // If there's a direct LoS, the generated path will be a straight line from start to target.
+    // Otherwise the pathfinding algorithm will be executed.
+    // If that option is not set, the pathfinding will be always executed.
+    UPROPERTY( config, EditAnywhere, Category = "PathFinding" )
+    TSubclassOf< USVORayCaster > DefaultLineOfSightClass;
 };
