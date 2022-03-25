9b7c3f5654aa75af8b410ab153a07e193bc21aa1
michael.delva <michael@emidee.net>
2022-03-25 14:03:30 +0100

    Bumped version

--- Source/SVONavigation/Public/SVOVersion.h
+++ Source/SVONavigation/Public/SVOVersion.h
@@ -6,7 +6,8 @@ enum class ESVOVersion : uint8
     NoVoxelExponent = 2,
     LeafNodeParent = 3,
     VolumeNavigationQueryFilter = 4,
+    NavigationDataChunks = 5,
 
-    MinCompatible = VolumeNavigationQueryFilter,
-    Latest = VolumeNavigationQueryFilter
+    MinCompatible = NavigationDataChunks,
+    Latest = NavigationDataChunks
 };
