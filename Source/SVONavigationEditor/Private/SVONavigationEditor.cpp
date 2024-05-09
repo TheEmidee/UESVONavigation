c45bb670a46fa553c51e9ad4db31e6c4a85953df
Viktor Blomme <viktor.blomme@gmail.com>
2024-05-09 19:15:08 +0200

    Remove include

--- Source/SVONavigationEditor/Private/SVONavigationEditor.cpp
+++ Source/SVONavigationEditor/Private/SVONavigationEditor.cpp
@@ -1,21 +1,19 @@
 #include "SVONavigationEditor/SVONavigationEditor.h"
 
-#include <PropertyEditorModule.h>
+IMPLEMENT_GAME_MODULE( FSVONavigationEditorModule, SVONavigationEditor );
 
-IMPLEMENT_GAME_MODULE(FSVONavigationEditorModule, SVONavigationEditor);
-
-DEFINE_LOG_CATEGORY(LogSVONavigationEditor)
+DEFINE_LOG_CATEGORY( LogSVONavigationEditor )
 
 #define LOCTEXT_NAMESPACE "SVONavigationEditor"
 
 void FSVONavigationEditorModule::StartupModule()
 {
-	UE_LOG(LogSVONavigationEditor, Verbose, TEXT("SVONavigationEditor: Module Startup"));
+    UE_LOG( LogSVONavigationEditor, Verbose, TEXT( "SVONavigationEditor: Module Startup" ) );
 }
 
 void FSVONavigationEditorModule::ShutdownModule()
 {
-	UE_LOG(LogSVONavigationEditor, Verbose, TEXT("SVONavigationEditor: Module Shutdown"));
+    UE_LOG( LogSVONavigationEditor, Verbose, TEXT( "SVONavigationEditor: Module Shutdown" ) );
 }
 
 #undef LOCTEXT_NAMESPACE
