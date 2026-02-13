90ef8f156c84a970d71a847c768061f9acc72570
Viktor Blomme <viktor.blomme@fishingcactus.com>
2026-02-13 17:28:59 +0100

    Add blueprint library

---
+++ Source/SVONavigation/SVOBlueprintFunctionLibrary.cpp
@@ -0,0 +1,47 @@
+﻿#include "SVOBlueprintFunctionLibrary.h"
+
+#include "SVONavigationData.h"
+
+#include <AI/NavigationSystemBase.h>
+#include <NavigationSystem.h>
+
+bool USVOBlueprintFunctionLibrary::IsActorInNavigableArea( const AActor * actor )
+{
+    if ( actor == nullptr )
+    {
+        return false;
+    }
+
+    const auto * world = actor->GetWorld();
+    if ( world == nullptr )
+    {
+        return false;
+    }
+
+    auto * nav_system = Cast< UNavigationSystemV1 >( world->GetNavigationSystem() );
+    if ( nav_system == nullptr )
+    {
+        return false;
+    }
+
+    const auto * nav_data = Cast< ASVONavigationData >( nav_system->GetNavDataForActor( *actor ) );
+    if ( nav_data == nullptr )
+    {
+        return false;
+    }
+
+    const TArray< FVector > points { actor->GetActorLocation() };
+    auto * volume_nav_data = nav_data->GetVolumeNavigationDataContainingPoints( points );
+    if ( volume_nav_data == nullptr )
+    {
+        return false;
+    }
+
+    FSVONodeAddress address;
+    if ( !volume_nav_data->GetNodeAddressFromPosition( address, actor->GetActorLocation() ) )
+    {
+        return false;
+    }
+
+    return volume_nav_data->IsNodeAddressNavigable( address );
+}
---
+++ Source/SVONavigation/SVOBlueprintFunctionLibrary.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <CoreMinimal.h>
+#include <Kismet/BlueprintFunctionLibrary.h>
+
+#include "SVOBlueprintFunctionLibrary.generated.h"
+
+UCLASS()
+class SVONAVIGATION_API USVOBlueprintFunctionLibrary final : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+    UFUNCTION( BlueprintPure, Category = "SVO Navigation" )
+    static bool IsActorInNavigableArea( const AActor * actor );
+};
